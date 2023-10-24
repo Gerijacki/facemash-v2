c6ece25687d51d86006b5af49cbd6430a4894186
Gerard Loriz <gerardloriz@ginebro.cat>
2023-10-24 16:23:20 +0200

    carpeta errors

---
+++ eduQuack/errors/1045.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>1045 | eduQuack</title>
+    <link rel="icon" href="images/ginebro-logo (1).png">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <div class="error-message">
+        <h1>1045 - Petició acceptada</h1>
+        <p>JBKJHIHGV</p>
+    </div>
+</body>
+
+</html>
---
+++ eduQuack/errors/2002.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>2002 | eduQuack</title>
+    <link rel="icon" href="images/ginebro-logo (1).png">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <div class="error-message">
+        <h1>2002 - Petició acceptada</h1>
+        <p>JBKJHIHGV</p>
+    </div>
+</body>
+
+</html>
